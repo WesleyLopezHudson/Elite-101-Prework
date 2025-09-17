151fd9d1197cc583ae00accbbd3493ef713240ae
WesleyLopezHudson <wesleylopezhudson@gmail.com>
2025-09-17 03:44:53 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#This is the chatbot project
+
+name = input ("Hello valued customer, welcome to [CONVIENCE STORE]. What is your name? ")
+age = input("And what is your age, " + name + "? ")
+opening_message = print("Is there anthing you need help with in the convenince store?") # Like a price or location of an item
+select_question = input("Actually to make things easier, here is a list of things you might need help with: "
+"Price of an item, "
+"Items in stock, "
+"Locations of items. "
+"To choose a question, reply with the name of the question you need help with. ")
